495a37d55324a5dd549c352d1459e31549545bb2
Kiran Anand <liteninkiran@hotmail.com>
2025-10-08 16:15:20 +0100

    Add dashboard & route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import type { Routes } from '@angular/router';
+import { Dashboard } from './pages/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'dashboard',
+        title: 'Dashboard',
+        component: Dashboard,
+    },
+    {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full',
+    },
+];
---
+++ src/app/pages/dashboard/dashboard.ts
@@ -0,0 +1,10 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+@Component({
+    selector: 'app-dashboard',
+    templateUrl: './dashboard.html',
+    styleUrl: './dashboard.scss',
+    imports: [],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+export class Dashboard {}
